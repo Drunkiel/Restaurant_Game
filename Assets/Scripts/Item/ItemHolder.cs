da0c4a935c209bb7aa90227f2b658c00a82e6734
Drunkiel <drunkielfel@gmail.com>
2024-01-24 17:22:58 +0100

    Update

--- Assets/Scripts/Item/ItemHolder.cs
+++ Assets/Scripts/Item/ItemHolder.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class ItemHolder : MonoBehaviour
 {
@@ -6,6 +7,9 @@ public class ItemHolder : MonoBehaviour
 
     public bool isHoldingItem;
     public bool isHoldingStackableItem;
+
+    [SerializeField] private ItemID holdingItem;
+    [SerializeField] private ItemID holdingStackableItem;
 
     [SerializeField] private Transform holderTransform;
     [SerializeField] private Transform itemPlaceTransform;
@@ -15,27 +19,66 @@ public class ItemHolder : MonoBehaviour
         instance = this;
     }
 
+    private void Update()
+    {
+        if (isHoldingItem) 
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) DropItem();
+        }
+    }
+
     public void PickItem(GameObject item)
     {
         if (!isHoldingItem)
         {
             GameObject newItem = Instantiate(item, itemPlaceTransform);
             newItem.transform.localScale /= 100;
+            newItem.transform.localPosition = Vector3.zero;
+            newItem.transform.localRotation = Quaternion.Euler(90f, 0, 0);
+            newItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            newItem.name = item.name;
+            holdingItem = newItem.GetComponent<ItemID>();
             itemPlaceTransform.GetChild(0).GetChild(1).gameObject.SetActive(false);
-
             isHoldingItem = true;
             Destroy(item);
             return;
         }
-        
+
         if (!isHoldingStackableItem)
         {
             GameObject newItem = Instantiate(item, holderTransform);
             newItem.transform.localScale /= 100;
+            newItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            newItem.name = item.name;
             holderTransform.GetChild(1).GetChild(1).gameObject.SetActive(false);
             isHoldingStackableItem = true;
             Destroy(item);
             return;
+        }
+    }
+
+    public void DropItem()
+    {
+        if (isHoldingItem)
+        {
+            itemPlaceTransform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+            Vector3 spawnPosition = transform.position + transform.forward * 1/4;
+            GameObject newItem = Instantiate(holdingItem.gameObject, spawnPosition, holdingItem.transform.rotation);
+            newItem.transform.localScale *= 100;
+            newItem.name = holdingItem.name;
+
+            //Unfreezing position
+            newItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            newItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+            Destroy(holdingItem.gameObject);
+            isHoldingItem = false;
+            return;
+        }
+
+        if (isHoldingStackableItem)
+        {
+
         }
     }
 }
